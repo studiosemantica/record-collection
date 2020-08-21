91c3708d61b42c02812cc27b3ae21151b270a3a6
LEA THOMAS <LEATHOMASLT@GMAIL.COM>
2020-08-21 11:57:32 -0400

    completed show by id controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   resource :users, only: [:create]
   post "/login", to: "users#login"
   get "/auto_login", to: "users#auto_login"
+
+  resources :artists do
+    resources :records do
+
+    end
+  end
+
+  get '/artists/:artist_id', to: 'artists#show'
+
 end
