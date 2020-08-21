d7fa1314be1d988607f8d76c8736ce9eacb8d2b8
STUDIO SEMANTICA <m90studio@tutanota.com>
2020-08-21 12:20:12 -0400

    Added artist search string controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,13 @@
 class ArtistsController < ApplicationController
+
+  def search
+    @artists = Artist.where('lower(name) LIKE lower(?)', "%#{ params[:search_string] }%")
+    render json: @artists
+  end
+
+
+  private
+  def artist_params
+    params.permit(:id, :title, :description, :search_string)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resource :users, only: [:create]
   post "/login", to: "users#login"
   get "/auto_login", to: "users#auto_login"
+
+  get '/artists/search/:search_string', to: 'artists#search'
 end
