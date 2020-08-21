20c3069bd811fddc67f4f3dbd110a0811ce034f3
LEA THOMAS <LEATHOMASLT@GMAIL.COM>
2020-08-21 12:47:47 -0400

    debugged routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # post "/login", to: "users#login"
   # get "/auto_login", to: "users#auto_login"
   
-  post '/artists', to: 'artists#create
+  post '/artists', to: 'artists#create'
   post "/login", to: "users#login"
   get "/auto_login", to: "users#auto_login"
   get '/artists/:artist_id', to: 'artists#show'
